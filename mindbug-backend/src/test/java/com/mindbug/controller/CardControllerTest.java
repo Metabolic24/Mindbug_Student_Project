64051834e91c28f99420d65c5fc8853422008699
Lepinay <elcirdec@gmail.com>
2025-02-05 18:23:16 +0100

    Add a new JSON file describing First Contact set #9

---
+++ mindbug-backend/src/test/java/com/mindbug/controller/CardControllerTest.java
@@ -0,0 +1,28 @@
+package com.mindbug.controller;
+
+import com.mindbug.model.Card;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CardControllerTest {
+
+    private CardController cardController;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        cardController = new CardController();
+    }
+
+    @Test
+    public void testGetCardsBySet_FirstContact() throws IOException {
+        List<Card> cards = cardController.getCardsBySet("First_Contact");
+
+        assertNotNull(cards, "The card list should not be null");
+
+        assertEquals(32, cards.size(), "The number of cards in the First_Contact set is incorrect");
+    }
+}
