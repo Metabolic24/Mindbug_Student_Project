5ae32d54ca5dc41f1ba2cf791976bd7dda36a2d3
huypqh11 <pqhuy20@vp.fitus.edu.vn>
2025-02-11 20:40:43 +0100

    chore Game.java Player.java

--- mindbug-backend/src/main/java/com/mindbug/models/Game.java
+++ mindbug-backend/src/main/java/com/mindbug/models/Game.java
@@ -22,7 +22,8 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "current_player_id")
+    @OneToOne
+    @JoinColumn(name = "current_player_id", referencedColumnName = "id")
     private Player currentPlayer;
 
     @OneToOne
@@ -32,4 +33,36 @@ public class Game {
     @OneToOne
     @JoinColumn(name = "player2_id", referencedColumnName = "id")
     private Player player2;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+    public void setCurrentPlayer(Player currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
 }
--- mindbug-backend/src/main/java/com/mindbug/models/Player.java
+++ mindbug-backend/src/main/java/com/mindbug/models/Player.java
@@ -12,7 +12,8 @@ import lombok.ToString;
 @Entity
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class Player implements Serializable {
-    
+
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -31,6 +32,27 @@ public class Player implements Serializable {
         // TODO: get lifepoints and mindbug from game configs
         this.lifepoints = 4;
         this.mindbug = 2;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getLifepoints() {
+        return lifepoints;
     }
 
 }
