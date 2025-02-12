0b2b59a12b43adbabb16c9a8f324e47b175467e0
mkotin <melaskotin@gmail.com>
2025-02-12 01:22:00 +0100

    add jacoco and checkstyle

--- mindbug-backend/src/main/java/com/mindbug/MindbugApplication.java
+++ mindbug-backend/src/main/java/com/mindbug/MindbugApplication.java
@@ -2,10 +2,7 @@ package com.mindbug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
