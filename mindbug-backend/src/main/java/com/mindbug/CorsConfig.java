6b74f030899ccd119bfecabd0b29a081ec64ed92
mkotin <melaskotin@gmail.com>
2025-02-18 00:33:25 +0100

    fixed cords

--- mindbug-backend/src/main/java/com/mindbug/CorsConfig.java
+++ mindbug-backend/src/main/java/com/mindbug/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.mindbug;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-               .allowedOriginPatterns("http://localhost:[*]") // Adaptez selon vos besoins
+               .allowedOriginPatterns("*")  // Adaptez selon vos besoins
                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                .allowedHeaders("*")
                .allowCredentials(true);
