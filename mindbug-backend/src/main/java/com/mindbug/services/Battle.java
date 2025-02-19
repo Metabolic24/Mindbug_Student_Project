e3327f0477bb935a7ec6f9ce17ef7c5757d96b44
mkotin <melaskotin@gmail.com>
2025-02-19 19:04:34 +0100

    update

--- mindbug-backend/src/main/java/com/mindbug/services/Battle.java
+++ mindbug-backend/src/main/java/com/mindbug/services/Battle.java
@@ -12,7 +12,20 @@ public class Battle {
     private GameSessionCard attackingCard;
     private GameSessionCard blockingCard;
 
+    private BattlePhase battlePhase;
+
+    public Battle() {
+        this.battlePhase = BattlePhase.NO_BATTLE;
+    }
+
+
+
     public void attack(GameSessionCard attackingCard) {
         this.attacking = attackingCard;
+        this.battlePhase = BattlePhase.ATTACKING;
+    } 
+
+    public boolean canAttack() {
+        if(this.battlePhase == BattlePhase.NO_BATTLE)
     }
 }
