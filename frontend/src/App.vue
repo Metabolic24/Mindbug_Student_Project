7478899c950ad83aaf3f6e39e8f7b24aaf9df06f
Victor <metabolic.ewilan@hotmail.fr>
2025-03-12 22:13:41 +0100

    Create an empty Vue3/Vite application

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <router-view/>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+   .app {
+     width: 1000px;
+     height: 800px;
+   }
+</style>
