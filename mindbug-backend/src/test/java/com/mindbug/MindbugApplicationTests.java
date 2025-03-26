67739bb0397aba1a595f34158516e977f29b4cbc
Lepinay <lnkc.github@gmail.com>
2025-03-26 17:09:33 +0100

    cleanup MindbugApplicationTest #73

--- mindbug-backend/src/test/java/com/mindbug/MindbugApplicationTests.java
+++ mindbug-backend/src/test/java/com/mindbug/MindbugApplicationTests.java
@@ -1,13 +1,27 @@
 package com.mindbug;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mindbug.controller.CardController;
+import com.mindbug.services.CardService;
 
 @SpringBootTest
 class MindbugApplicationTests {
 
+	@Autowired
+    private CardController cardController;
+
+    @Autowired
+    private CardService cardService;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(cardController, "The controller should not be null");
+        assertNotNull(cardService, "The Service should not be null");
 	}
 
 }
