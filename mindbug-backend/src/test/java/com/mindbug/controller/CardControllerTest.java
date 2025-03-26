1796f54524777961318ee5f5e981c2cdf811b3a6
Lepinay <lnkc.github@gmail.com>
2025-03-26 17:16:23 +0100

    cleanup GameServerTest by division in multiple test #73

--- mindbug-backend/src/test/java/com/mindbug/controller/CardControllerTest.java
+++ mindbug-backend/src/test/java/com/mindbug/controller/CardControllerTest.java
@@ -1,7 +1,6 @@
 package com.mindbug.controller;
 
 import com.mindbug.models.Card;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
