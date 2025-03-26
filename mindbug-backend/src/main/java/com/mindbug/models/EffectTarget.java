0db9b8488d310b57ba3434b84b54f4189469b146
Lepinay <lnkc.github@gmail.com>
2025-03-26 13:37:53 +0100

    cleanup Effect and creation of enum #73

---
+++ mindbug-backend/src/main/java/com/mindbug/models/EffectTarget.java
@@ -0,0 +1,11 @@
+package com.mindbug.models;
+
+public enum EffectTarget {
+    SELF,
+    OPPONENT,
+    ALL,
+    OPPONENT_CREATURE,
+    OPPONENT_HAND,
+    ALLIES,
+    DISCARD,
+}
---
+++ mindbug-backend/src/main/java/com/mindbug/models/OperationType.java
@@ -0,0 +1,6 @@
+package com.mindbug.models;
+
+public enum OperationType {
+    SET,
+    EQUALIZE,
+}
