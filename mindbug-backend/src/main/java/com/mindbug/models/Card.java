c333d4d8184c612345402fb21f20aac75146755b
Lepinay <lnkc.github@gmail.com>
2025-03-26 16:53:06 +0100

    cleanup CardList + enum to JSON name adaptator #73

--- mindbug-backend/src/main/java/com/mindbug/models/Card.java
+++ mindbug-backend/src/main/java/com/mindbug/models/Card.java
@@ -5,9 +5,16 @@ import lombok.Setter;
 
 import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 @Getter
 @Setter
+@Entity
 public class Card {
+
+    @Id
+    private Long id;
 
     private String name;
     private int copies;
