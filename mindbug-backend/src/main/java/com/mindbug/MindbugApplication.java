4251de039e8cf60dcbc2122e23ca5cd955ba002a
Lepinay <lnkc.github@gmail.com>
2025-03-26 15:46:07 +0100

    cleanup MindbugApplication #73

--- mindbug-backend/src/main/java/com/mindbug/MindbugApplication.java
+++ mindbug-backend/src/main/java/com/mindbug/MindbugApplication.java
@@ -2,7 +2,6 @@ package com.mindbug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,10 +10,5 @@ public class MindbugApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MindbugApplication.class, args);
-	}
-
-	@RequestMapping("/")
-	String home() {
-		return "Hello World now!";
 	}
 }
