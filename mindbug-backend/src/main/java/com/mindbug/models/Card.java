891b8705f4fdf543a13bbb046ebcd15a5ed147b7
mkotin <melaskotin@gmail.com>
2025-04-10 13:56:10 +0200

    fix bug card model does not have auto generated id #73

--- mindbug-backend/src/main/java/com/mindbug/models/Card.java
+++ mindbug-backend/src/main/java/com/mindbug/models/Card.java
@@ -6,6 +6,8 @@ import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
@@ -17,6 +19,7 @@ import lombok.Setter;
 public class Card {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
