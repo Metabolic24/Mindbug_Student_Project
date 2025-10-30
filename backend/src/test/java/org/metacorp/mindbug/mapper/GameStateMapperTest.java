b114ed281fc310aa394b5c4d2b3ea3fe6a732319
Metabolic24 <metabolic.ewilan@hotmail.fr>
2025-10-30 01:26:25 +0100

    Add new unit tests to improve code coverage

---
+++ backend/src/test/java/org/metacorp/mindbug/mapper/GameStateMapperTest.java
@@ -0,0 +1,82 @@
+package org.metacorp.mindbug.mapper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.metacorp.mindbug.dto.CardDTO;
+import org.metacorp.mindbug.dto.GameStateDTO;
+import org.metacorp.mindbug.dto.PlayerDTO;
+import org.metacorp.mindbug.exception.UnknownPlayerException;
+import org.metacorp.mindbug.model.Game;
+import org.metacorp.mindbug.model.card.CardInstance;
+import org.metacorp.mindbug.model.player.Player;
+import org.metacorp.mindbug.service.GameService;
+import org.metacorp.mindbug.service.PlayerService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameStateMapperTest {
+
+    private final GameService gameService = new GameService();
+    private Game game;
+
+    @BeforeEach
+    public void setUp() throws UnknownPlayerException {
+        Player player1 = new Player(PlayerService.createPlayer("player1"));
+        Player player2 = new Player(PlayerService.createPlayer("player2"));
+        game = gameService.createGame(player1.getUuid(), player2.getUuid());
+    }
+
+    @Test
+    public void fromGame_startedGame() {
+        GameStateDTO gameStateDTO = GameStateMapper.fromGame(game);
+
+        assertNotNull(gameStateDTO);
+        assertEquals(game.getUuid(), gameStateDTO.getUuid());
+
+        comparePlayers(game.getCurrentPlayer(), gameStateDTO.getPlayer());
+        comparePlayers(game.getOpponent(), gameStateDTO.getOpponent());
+
+        assertNull(gameStateDTO.getCard());
+        assertNull(gameStateDTO.getWinner());
+        assertNull(gameStateDTO.getChoice());
+    }
+
+    private void comparePlayers(Player player, PlayerDTO playerDTO) {
+        assertEquals(player.getUuid(), playerDTO.getUuid());
+        assertEquals(player.getName(), playerDTO.getName());
+        assertEquals(player.getMindBugs(), playerDTO.getMindbugCount());
+        assertEquals(player.getDrawPile().size(), playerDTO.getDrawPileCount());
+        assertEquals(player.getTeam().getLifePoints(), playerDTO.getLifePoints());
+        assertEquals(player.getDisabledTiming(), playerDTO.getDisabledTiming());
+
+        compareCards(player.getHand(), playerDTO.getHand());
+        compareCards(player.getBoard(), playerDTO.getBoard());
+        compareCards(player.getDiscardPile(), playerDTO.getDiscard());
+    }
+
+    private void compareCards(List<CardInstance> playerHand, List<CardDTO> playerDTOHand) {
+        assertEquals(playerHand.size(), playerDTOHand.size());
+
+        for (CardInstance playerCard : playerHand) {
+            boolean found = false;
+
+            for (CardDTO playerDTOCard : playerDTOHand) {
+                if (playerCard.getUuid().equals(playerDTOCard.getUuid())) {
+                    assertEquals(playerCard.getCard().getName(), playerDTOCard.getName());
+                    assertEquals(playerCard.getPower(), playerDTOCard.getPower());
+                    assertEquals(playerCard.getCard().getId(), playerDTOCard.getId());
+                    assertEquals(playerCard.getKeywords(), playerDTOCard.getKeywords());
+                    assertEquals(playerCard.getCard().getSetName(), playerDTOCard.getSetName());
+                    assertEquals(playerCard.getOwner().getUuid(), playerDTOCard.getOwnerId());
+
+                    found = true;
+                    break;
+                }
+            }
+
+            assertTrue(found);
+        }
+    }
+}
--- backend/src/test/java/org/metacorp/mindbug/service/GameServiceTest.java
+++ backend/src/test/java/org/metacorp/mindbug/service/GameServiceTest.java
@@ -2,6 +2,7 @@ package org.metacorp.mindbug.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.metacorp.mindbug.exception.UnknownPlayerException;
 import org.metacorp.mindbug.model.CardSetName;
 import org.metacorp.mindbug.model.Game;
 import org.metacorp.mindbug.model.card.CardInstance;
@@ -15,6 +16,7 @@ import org.metacorp.mindbug.utils.CardUtils;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,11 +24,62 @@ public class GameServiceTest {
 
     private Game game;
     private Player currentPlayer;
+    private GameService gameService;
 
     @BeforeEach
     public void initGame() {
-        game = StartService.newGame(new Player("Player1"), new Player("Player2"));
+        gameService = new GameService();
+        game = StartService.newGame(new Player(PlayerService.createPlayer("Player1")), new Player(PlayerService.createPlayer("Player2")));
         currentPlayer = game.getCurrentPlayer();
+    }
+
+    @Test
+    public void createGame_nominal() throws UnknownPlayerException {
+        List<Player> players = game.getPlayers();
+        game = gameService.createGame(players.get(0).getUuid(), players.get(1).getUuid());
+
+        assertNotNull(game);
+        assertNotNull(game.getUuid());
+    }
+
+    @Test
+    public void findById_nominal() throws UnknownPlayerException {
+        List<Player> players = game.getPlayers();
+        game = gameService.createGame(players.get(0).getUuid(), players.get(1).getUuid());
+
+        assertEquals(game, gameService.findById(game.getUuid()));
+    }
+
+    @Test
+    public void findById_badGame() {
+        assertNull(gameService.findById(UUID.randomUUID()));
+    }
+
+    @Test
+    public void endGame_nominal() throws UnknownPlayerException {
+        List<Player> players = game.getPlayers();
+        UUID loserId = players.get(0).getUuid();
+        UUID winnerId = players.get(1).getUuid();
+        game = gameService.createGame(loserId, winnerId);
+
+        gameService.endGame(loserId, game.getUuid());
+        assertEquals(winnerId, game.getWinner().getUuid());
+    }
+
+    @Test
+    public void endGame_badGame() {
+        gameService.endGame(UUID.randomUUID(), UUID.randomUUID());
+        assertNull(game.getWinner());
+    }
+
+    @Test
+    public void endGame_badPlayer() throws UnknownPlayerException {
+        List<Player> players = game.getPlayers();
+        UUID loserId = players.get(0).getUuid();
+        game = gameService.createGame(loserId, players.get(1).getUuid());
+
+        gameService.endGame(UUID.randomUUID(), game.getUuid());
+        assertNull(game.getWinner());
     }
 
     @Test
@@ -122,8 +175,8 @@ public class GameServiceTest {
     @Test
     public void refreshGameState_multiplePassiveEffects() {
         // Create a new game manually, as we need to get some specific cards
-        Player currentPlayer = new Player("player1");
-        Player opponent =  new Player("player2");
+        Player currentPlayer = new Player(PlayerService.createPlayer("player1"));
+        Player opponent =  new Player(PlayerService.createPlayer("player2"));
         game = new Game(currentPlayer, opponent);
         game.setCurrentPlayer(currentPlayer);
         game.setCards(CardUtils.getCardsFromConfig(CardSetName.FIRST_CONTACT.getKey()));
