3cfdfa866c6f333d68c811089fe3ac98ed0e8bf6
Metabolic24 <metabolic.ewilan@hotmail.fr>
2025-12-18 01:27:46 +0100

    Add new ForceAttack effect

---
+++ backend/src/main/java/org/metacorp/mindbug/model/effect/impl/ForceAttackEffect.java
@@ -0,0 +1,17 @@
+package org.metacorp.mindbug.model.effect.impl;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.metacorp.mindbug.model.card.CardKeyword;
+import org.metacorp.mindbug.model.effect.GenericEffect;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+public class ForceAttackEffect extends GenericEffect {
+    public final static String TYPE = "FORCE_ATTACK";
+
+    private CardKeyword keyword;    // only cards with this keyword are affected
+    private boolean singleTarget;   // attack target can only be the effect source card
+}
